81e01ea73ac5622d868437bcc210cee26018196f
anonymousdomain <domainanonymous1@gmail.com>
2021-11-09 00:29:54 +0300

    smplified line of code

--- DecimalToBinary.java
+++ DecimalToBinary.java
@@ -1,14 +1,13 @@
 public class DecimalToBinary {
 
   public static String findBinary(int decimal, String result) {
-
-    if(decimal==0){
-        return result;
+    if (decimal == 0) {
+      return result;
     }
-    return result=findBinary(decimal/2, result)+decimal%2;
+    return result = findBinary(decimal / 2, result) + decimal % 2;
   }
 
-public static void main(String[] args) {
-    System.out.println(DecimalToBinary.findBinary(233,""));
-}
+  public static void main(String[] args) {
+    System.out.println(DecimalToBinary.findBinary(233, ""));
+  }
 }
